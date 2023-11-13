2242f08c1ef2de1c391e7134369653dba66f8715
Leonardo Bongermino <leonardo.b8@SCHULE.KOELN>
2023-11-13 12:59:29 +0100

    Die ersten beiden UnitTests

---
+++ Verlag/Buch.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Verlag
+{
+    public class Buch
+    {
+        private string titel;
+        private string autor;
+        private int auflage;
+
+        public Buch(string autor, string titel, int auflage)
+        {
+            this.titel = titel;
+            this.autor = autor;
+            this.auflage = auflage;
+
+        }
+
+        public Buch(string autor, string titel)
+        {
+            this.titel = titel;
+            this.autor = autor;
+            this.auflage = 1;
+        }
+
+        public string Titel
+        {
+            set { titel = value; }
+            get { return titel; }
+        }
+
+        public string Autor
+        {
+            set { autor = value; }
+            get { return autor; }
+        }
+
+        public int Auflage
+        {
+            set {  auflage = value; }
+            get { return auflage; }
+
+        }
+
+
+    }
+
+}
+
+
+
+
+
+	
