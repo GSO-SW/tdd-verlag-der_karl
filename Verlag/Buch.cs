0a185c75ed319da34bd81012e578673e728fdb78
Kerem Emen <Kerem.e5@so.schule.koeln>
2023-11-13 12:33:18 +0100

    Zwei Konstruktoren erstellt. Code läuft!

---
+++ Verlag/Buch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verlag
+{
+    public class Buch
+    {
+        private string autor;
+        private string titel;
+        private int auflage;
+
+        public Buch(string autor, string titel, int auflage):this(autor, titel)
+        {
+            this.auflage = auflage;
+        }
+        public Buch(string autor, string titel)
+        {
+            this.autor = autor;
+            this.titel = titel;
+            auflage = 1;
+
+
+        }
+        public string Autor
+        {
+            get { return autor; }
+            set { autor = value; }
+        }
+
+        public string Titel
+        {
+            get { return titel; } 
+            set { titel = value; }
+        }
+        
+        public int Auflage
+        {
+            get { return auflage; }
+            set { auflage = value; }
+        } 
+    }
+}
