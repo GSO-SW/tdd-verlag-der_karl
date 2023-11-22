717f3bd5bc42ae32aedf2080bb522c691a0d8e7e
azarialii <alireza.a3@gso.schule.koeln>
2023-11-22 17:38:02 +0100

    22.11

--- Verlag/Buch.cs
+++ Verlag/Buch.cs
@@ -12,6 +12,9 @@ namespace Verlag
         private string titel;
         private int auflage;
         private string autorNeu;
+        private string iSBN;
+        private string iSBN13;
+        private string iSBN10;
 
         public Buch(string autor, string titel, int auflage):this(autor, titel)
         {
@@ -45,6 +48,55 @@ namespace Verlag
             get { return autoNeu; }
             set { autoNeu = value; }
         }
-
+        public string ISBN
+        {
+            get { return iSBN; }
+            set {
+                iSBN = value;
+            }
+        }
+        public string ISBN10
+        {
+            get {
+                
+                    iSBN10 = iSBN.Substring(4,11).Replace("-", "");
+                    int x = 10;
+                    int count = 0;                   
+                    for (int i = 0; i < iSBN10.Length; i++)
+                    {
+                        count += Convert.ToInt32(iSBN10[i]) * x;
+                        
+                        x += -1;
+                    }
+                    count = 11 - (count % 11);
+                    iSBN10 += count;
+                    return iSBN10;
+                
+            }
+        }
+        public string ISBN13 
+        {
+            get 
+            {
+                if (iSBN.Length == 17)
+                {                   
+                    iSBN13 = iSBN.Replace("-", "");
+                }
+                else if (iSBN.Length == 16)
+                {
+                    int x = 1;
+                    int count = 0;
+                    iSBN13 = iSBN.Replace("-", "");
+                    for (int i = 0; i < 16; i++)
+                    {
+                        if (x == 1) count += Convert.ToInt32(iSBN13[i]) * 3;
+                        else count += Convert.ToInt32(iSBN13[i]);
+                        x = x * -1;
+                    }
+                    iSBN13 += count;
+                }
+                return iSBN13;
+            }
+        }
     }
 }
